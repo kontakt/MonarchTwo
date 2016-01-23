4f10c634a46987a6a45db4360964bf3e72505adf
Connor Huffine <chuffine@gmail.com>
2016-01-22 19:49:30 -0500

    Add license and second library

--- Arduino/sensors/sensors.ino
+++ Arduino/sensors/sensors.ino
@@ -1,4 +1,19 @@
-#include "libraries/SparkFun_LSM9DS1/SparkFunLSM9DS1.h"
+/* ODU Monarch Two Sensor Package Code
+ *  
+ *  This code handles sensor data collection from a package of sensors
+ *  onboard the Monarch Two suborbital rocket payload, and transfers them
+ *  to a smartphone for data transmission to a groundstation.
+ *  
+ *  All code is provided under the BSD license. See license.txt.
+ *  
+ *  Authors:
+ *    Connor Huffine
+ *  
+ */
+
+#include "libraries/SparkFun_LSM9DS1/SparkFunLSM9DS1.h"   // 9DOF Sensor
+#include "libraries/Adafruit_ADS1X15/Adafruit_ADS1015.h"  // ADC
+
 void setup() {
   // put your setup code here, to run once:
 
